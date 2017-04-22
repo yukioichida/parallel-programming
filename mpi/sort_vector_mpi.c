cce965c75d54f1de1f9bbf4e366103a560a738ea
Alexandre Yukio Ichida <yukioichida@gmail.com>
2017-04-22 00:21:03 -0300

    tempos calculados

--- mpi/sort_vector_mpi.c
+++ mpi/sort_vector_mpi.c
@@ -7,23 +7,19 @@
 #define MASTER    0    // id do mestre
 #define POISON_PILL -2
 #define FIRST_TASK -1
-
 #define ARRAY_MSG 2 // tipo de mensagem que transmite um array
-#define INDEX_MSG 1
+#define INDEX_MSG 1 // tipo de mensagem que transmite um índice
 
-
+/* Função de que é usado pelo qsort */
 int cmpfunc (const void * a, const void * b){
    return ( *(int*)a - *(int*)b );
 }
-
-/**
-Versão onde o escravo requisita as tarefas
-*/
+/* Método onde é o escravo que requisita a tarefa */
 int main(int argc,char **argv){
 
-  int n_tasks, task_id, exit_code, i, j, index, worker, task_type = 0, array_to_send = 0, recv_arrays = 0;
+  int n_tasks, task_id, exit_code, i, j, index, worker, array_to_send = 0, recv_arrays = 0;
+  double t1, t2;  
   MPI_Status mpi_status;
-
   exit_code = MPI_Init(&argc,&argv);
   exit_code|= MPI_Comm_size(MPI_COMM_WORLD,&n_tasks);
   exit_code|= MPI_Comm_rank(MPI_COMM_WORLD,&task_id);
@@ -33,18 +29,22 @@ int main(int argc,char **argv){
     return 1;
   }
 
-  if (task_id == MASTER){        
-
+  if (task_id == MASTER){
+    // ====================== MESTRE ============================
+    t1 = MPI_Wtime();
+    // Aloca as matrizes
     int (*bag_of_tasks)[N_ARRAYS] = malloc (ARRAY_SIZE * sizeof *bag_of_tasks);        
     int (*results)[N_ARRAYS] = malloc (ARRAY_SIZE * sizeof *results);
 
+    // populando nros invertidos
     for (i = 0; i < N_ARRAYS; i++){
       for(j=0; j < ARRAY_SIZE; j++){
-        bag_of_tasks [i][j] = (ARRAY_SIZE-j-1)*(1+i); // populando nros invertidos
+        bag_of_tasks [i][j] = (ARRAY_SIZE-j-1); 
       }
     }
 
-    while(recv_arrays < N_ARRAYS) { // Delega enquanto tem arrays para receber
+    // Delega enquanto tem arrays para receber
+    while(recv_arrays < N_ARRAYS) { 
       for (worker = 1; worker < n_tasks; worker++){  // Recebe as requisições de trabalho...
         if (recv_arrays < N_ARRAYS) {
           MPI_Recv(&index, 1, MPI_INT, worker, INDEX_MSG, MPI_COMM_WORLD, &mpi_status);
@@ -53,8 +53,9 @@ int main(int argc,char **argv){
               recv_arrays++;
           }
         }
-      }      
-      for(worker = 1; worker < n_tasks; worker++){ // ... e envia mais vetores
+      }
+      // ... e envia mais vetores
+      for(worker = 1; worker < n_tasks; worker++){ 
         if (array_to_send < N_ARRAYS){
           MPI_Send(&array_to_send, 1, MPI_INT, worker, INDEX_MSG, MPI_COMM_WORLD);
           MPI_Send(&bag_of_tasks[array_to_send], ARRAY_SIZE, MPI_INT, worker, ARRAY_MSG, MPI_COMM_WORLD);
@@ -62,33 +63,26 @@ int main(int argc,char **argv){
         }
       }           
     }
-   
-    task_type = POISON_PILL; // enviando POISON PILL para matar os escravos
+
+    index = POISON_PILL; // enviando POISON PILL para matar os escravos
     for (worker = 1; worker < n_tasks; worker++){
-      MPI_Send(&task_type, 1, MPI_INT, worker, INDEX_MSG, MPI_COMM_WORLD);
+      MPI_Send(&index, 1, MPI_INT, worker, INDEX_MSG, MPI_COMM_WORLD);
     }
 
-    /*printf("[MASTER] Resultados:\n");
-    for(i = 0; i < N_ARRAYS; i++){
-      printf("Array %d: [", i);
-      for (j = 0; j < ARRAY_SIZE; j++){
-        printf("%d ", results[i][j]);
-      }
-      printf("]\n");
-    }
-    fflush(stdout);
-*/
     free(bag_of_tasks);
     free(results);
+    t2 = MPI_Wtime(); 
+    printf("[Master] Duration [%f]\n", t2-t1);
+
   } else {
+
+    t1 = MPI_Wtime();
     // ================ SLAVE ===================
     int alive = 1;
     int array[ARRAY_SIZE];
     int index = FIRST_TASK;
-    
-    /* Já pede uma tarefa já de inicio */
-    MPI_Send(&index, 1, MPI_INT, MASTER, INDEX_MSG, MPI_COMM_WORLD); 
-    
+
+    MPI_Send(&index, 1, MPI_INT, MASTER, INDEX_MSG, MPI_COMM_WORLD); // Ja pede uma tarefa de inicio
     do {
       MPI_Recv(&index, 1, MPI_INT, MASTER, 1, MPI_COMM_WORLD, &mpi_status);
       if (index == POISON_PILL) {
@@ -101,7 +95,11 @@ int main(int argc,char **argv){
         MPI_Send(&index, 1, MPI_INT, MASTER, INDEX_MSG, MPI_COMM_WORLD); // ... e envia para o mestre
         MPI_Send(&array, ARRAY_SIZE, MPI_INT, MASTER, ARRAY_MSG, MPI_COMM_WORLD);
       }
-    } while (alive != 0);        
+    } while (alive != 0);
+
+    t2 = MPI_Wtime(); 
+    printf("[Worker %d] Duration [%f]\n", task_id, t2-t1);
+
   }
 
   MPI_Finalize();
