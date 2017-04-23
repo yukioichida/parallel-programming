32535937ad12eb988c4f399eba05e2538c2f60e3
Alexandre Yukio Ichida <yukioichida@gmail.com>
2017-04-22 21:03:32 -0300

    duration sequential

--- mpi/sort_vector_sequential.c
+++ mpi/sort_vector_sequential.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define ARRAY_SIZE 5 //Tamanho do array
-#define N_ARRAYS  4 // Quantidade de arrays
+#define ARRAY_SIZE 100000 //Tamanho do array
+#define N_ARRAYS  1000 // Quantidade de arrays
 
 /* Função de que é usado pelo qsort */
 int cmpfunc (const void * a, const void * b){
@@ -10,6 +11,8 @@ int cmpfunc (const void * a, const void * b){
 }
 
 int main(int argc,char **argv){
+	clock_t begin = clock();
+
 	int i, j;
 	// Aloca as matrizes
   int (*bag_of_tasks)[ARRAY_SIZE] = malloc(N_ARRAYS * sizeof *bag_of_tasks);
@@ -20,25 +23,30 @@ int main(int argc,char **argv){
     }
   }
   
+  /*
   for (i = 0; i < N_ARRAYS; i++){
   	printf("Array %d : [", i);
     for(j = 0; j < ARRAY_SIZE; j++){
       printf("%d ", bag_of_tasks[i][j]);
     }
     printf("]\n");
-  }
+  }*/
   
   for (i = 0; i < N_ARRAYS; i++){
   	qsort(bag_of_tasks[i], ARRAY_SIZE, sizeof(int), cmpfunc);// ... trabalha...  
 	}
 
+	/*
   for (i = 0; i < N_ARRAYS; i++){
   	printf("Array %d : [", i);
     for(j=0; j < ARRAY_SIZE; j++){
       printf("%d ", bag_of_tasks [i][j]);
     }
     printf("]\n");
-  }
+  }*/
 
   free(bag_of_tasks);
+  clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	printf("[Sequential] Duration [%f]\n", time_spent);
 }
